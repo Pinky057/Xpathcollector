fe99607ebb21b36bb2fee89db130308f0cac4f87
Ayesha Akhter <ayesha.akhter@testsprint360.com>
2024-01-18 07:30:53 +0000

    Squashed commit of the following
    commit f95ea243 
    Author: Ayesha Akhter <munmun@ayeshas-mbp.lan> 
    Date: Tue Jan 16 2024 19:42:16 GMT-0800 (Pacific Standard Time) 
    
        TES-46: added new 9 page level actions
    
    
    commit 1958c931 
    Author: Ayesha Akhter <munmun@ayeshas-mbp.lan> 
    Date: Mon Jan 15 2024 19:58:52 GMT-0800 (Pacific Standard Time) 
    
        TES-46: Component actions to be added in page level actions (in addition to the element level actions)

--- display.js
+++ display.js
@@ -1,7 +1,15 @@
 // display.js
 
-window.onload = function() {
+var bkg = chrome.extension.getBackgroundPage();
+
+window.onload = function () {
     var savedItems = JSON.parse(localStorage.getItem("selectedItems"));
     var displayElement = document.getElementById("display");
     displayElement.innerHTML = savedItems ? savedItems.join(", ") : "No items have been saved.";
 }
+
+// Listener for Generate method button
+document.getElementById("generateMethod").addEventListener("click", function () {
+    //TODO: Need to save
+    bkg.console.log("Getting Methods for display ");
+})
